5c6357bae87672ae92e11fe1b51bbe3b31696f9e
ianyimi <41877924+ianyimi@users.noreply.github.com>
2021-09-06 20:13:28 -0700

    Dev (#2)
    * src folder w index file
    
    * hello world
    
    * hello world with onClick
    
    * hello world CLEANUP lol and more interaction added
    
    * hello world CLEANUP 2 wow

---
+++ src/World/helloWorld/index.tsx
@@ -0,0 +1,67 @@
+import {useEffect, useRef, useState} from "react";
+import {animated, useSpring} from "@react-spring/three";
+import {Interactable, useLimiter, usePlayer} from "spacesvr";
+import {useFrame} from "@react-three/fiber";
+import {Text} from "@react-three/drei";
+import * as THREE from "three";
+
+export default function Index() {
+
+  const spinningBox = useRef();
+  const [hover, setHover] = useState(false);
+  const [select, setSelect] = useState(false);
+  const { matColor, posY } = useSpring({
+    matColor: hover ? "blue" : "red",
+    posY: select ? 2 : 0,
+    config: {
+      mass: 1
+    }
+  })
+
+  const { raycaster } = usePlayer();
+  useEffect(() => {
+    console.log(raycaster)
+    if (raycaster) raycaster.far = 10;
+  }, [raycaster])
+
+  const limiter = useLimiter(45);
+  useFrame(({ clock }) => {
+    if (!limiter.isReady(clock) || !spinningBox.current) return;
+    spinningBox.current.rotation.x = clock.getElapsedTime()/2;
+    spinningBox.current.rotation.y = clock.getElapsedTime()/2;
+  })
+
+  return (
+    <group>
+      <ambientLight intensity={0.5} />
+      <Text
+        color="black"
+        fontSize={1}
+        position-y={1}
+        rotation-y={-Math.PI/2}
+      >
+        Hello World
+      </Text>
+      <Interactable
+        onHover={() => setHover(true)}
+        onUnHover={() => setHover(false)}
+        onClick={() => setSelect(!select)}
+      >
+        <animated.group position-y={posY}>
+          <mesh ref={spinningBox}>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <animated.meshBasicMaterial color={matColor} />
+          </mesh>
+        </animated.group>
+      </Interactable>
+      <mesh name="sphere">
+        <sphereBufferGeometry args={[10, 30, 30]} />
+        <meshStandardMaterial color="blue" opacity={0.25} transparent={true} side={THREE.DoubleSide} />
+      </mesh>
+      <mesh rotation-x={-Math.PI/2}>
+        <planeBufferGeometry args={[100, 100]} />
+        <meshBasicMaterial color="green" />
+      </mesh>
+    </group>
+  )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,83 @@
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const AUTHOR = "Isaiah Anyimi",
+    TITLE = "Zay's World";
+
+  return (
+    <>
+      <Head>
+        {/* Basic Info*/}
+        <meta charSet="utf-8" />
+        <meta name="language" content="english" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="content-type" content="text/html" />
+        <title>{TITLE}</title>
+
+        {/* SEO tags */}
+        {/*<meta name="author" content={AUTHOR} />*/}
+        {/*<meta name="designer" content={AUTHOR} />*/}
+        {/*<meta name="publisher" content={AUTHOR} />*/}
+        {/*<meta*/}
+        {/*  name="description"*/}
+        {/*  content="Muse 3D websites are your portal to the future of the web."*/}
+        {/*/>*/}
+        {/*<meta name="keywords" content="Muse" />*/}
+        {/*<meta name="distribution" content="web" />*/}
+
+        {/* open graph */}
+        {/*<meta property="og:url" content="https://universe.muse.place" />*/}
+        {/*<meta property="og:type" content="website" />*/}
+        {/*<meta*/}
+        {/*  property="og:title"*/}
+        {/*  content="Muse | Instantly build and deploy your own World"*/}
+        {/*/>*/}
+        {/*<meta*/}
+        {/*  property="og:description"*/}
+        {/*  content="Use our advanced customization features to add in 2D and 3D assets into your world."*/}
+        {/*/>*/}
+        {/*<meta property="og:image" content="icons/icon-512x512.png" />*/}
+
+        {/* pwa */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="icons/icon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="icons/icon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#808080" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+
+        {/* mobile */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* twitter */}
+        {/*<meta name="twitter:card" content="Muse" />*/}
+        {/*<meta name="twitter:site" content="@musehq_" />*/}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+export default MyApp
