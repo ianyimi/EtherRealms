5965104dc2ea536c2f54e2d3c83dedff8cc039f7
ianyimi <anyiminene@gmail.com>
2021-11-20 04:07:24 -0800

    fixed videos not working. still double clicks nfts

--- src/Realm/properties/RealmScene/Cubes/DisplayCubes/DisplayCube.tsx
+++ src/Realm/properties/RealmScene/Cubes/DisplayCubes/DisplayCube.tsx
@@ -2,23 +2,24 @@ import { GroupProps } from "@react-three/fiber";
 import { Box, Sphere } from "@react-three/drei";
 import Media from "../../components/Media";
 import { useRealm } from "Realm/components/RealmState";
-import { useRef } from "react";
+import {ReactNode, useRef} from "react";
 import { useHelper } from "@react-three/drei";
 import { SpotLightHelper } from "three";
 
-export default function DisplayCube(props: { sources: string[], links: string[] } & GroupProps) {
+export default function DisplayCube(props: { assets: Record<string, unknown>[] } & GroupProps) {
 
-  const { sources, links, ...restProps } = props;
+  const { assets, ...restProps } = props;
   const { scene: { theme = "Red" } } = useRealm();
   const light = useRef()
 
   // useHelper(light, SpotLightHelper, "teal")
 
-  const images = [];
-  for (let i=0; i<sources.length; i++) {
+  const images: ReactNode[] = [];
+  for (let i=0; i<assets.length; i++) {
+    const src = assets[i].animation_url && (assets[i].animation_url as string).endsWith(".mp4") ? assets[i].animation_url as string : assets[i].image_url as string;
     images.push(
       <group rotation-y={2*i*Math.PI/4} key={i}>
-        <Media src={sources[i]} link={links[i]} position-z={1.1} />
+        <Media src={src} link={assets[i].permalink as string} position-z={1.1} />
         {/*<spotLight ref={light} position={[0, 1, 10]} intensity={0.1} distance={50} castShadow />*/}
       </group>
     )
--- src/Realm/properties/RealmScene/Cubes/DisplayCubes/index.tsx
+++ src/Realm/properties/RealmScene/Cubes/DisplayCubes/index.tsx
@@ -14,7 +14,7 @@ export default function DisplayCubes() {
   useEffect(() => {
     console.log("fetch")
     // fetchAssets("0xA2bF505e8AC95856C3a7F454374C08ad83B8612A").then((assets) => {
-    fetchAssets("0x80207b6ef45dcd6e2d2f5bf692320c8b46b6bf09").then((assets) => {
+    fetchAssets("0xb34af2448f1789ae942f592e8770da5c0293a15d").then((assets) => {
       if (setAssets) {
         setAssets(assets)
       }
@@ -28,25 +28,13 @@ export default function DisplayCubes() {
       if (assets.length-i<4) {
         break;
       }
-      if (i>40) break;
-      // if (!assets[1] || !assets[i+1] || assets[1+2] || assets[i+3]) break;
-      const imgSources = [
-        assets[i].animation_url || assets[i].image_url,
-        assets[i+1].animation_url || assets[i+1].image_url,
-        assets[i+2].animation_url || assets[i+2].image_url,
-        assets[i+3].animation_url || assets[i+3].image_url
-      ],
-        links = [
-          assets[i].permalink,
-          assets[i+1].permalink,
-          assets[i+2].permalink,
-          assets[i+3].permalink
-        ];
+      // if (i>40) break;
+
       cubes.push(
         <group rotation-y={assets.length%4 === 0 ? 2*i*Math.PI/(assets.length) : 2*i*Math.PI/(assets.length-1)} key={i}>
           <group position-z={-10}>
             {/*<DisplayCube sources={imageSources[i]} position-y={1} />*/}
-            <DisplayCube sources={imgSources as string[]} links={links as string[]} position-y={1} />
+            <DisplayCube assets={[assets[i], assets[i+1], assets[i+2], assets[i+3]]} position-y={1} />
           </group>
         </group>
       )
