88ee5204e87ef34bf90f9bdf47f00afd15d2eaf8
ianyimi <anyiminene@gmail.com>
2021-12-24 23:37:27 -0800

    volumetric lights removed dead code

--- src/Realm/properties/PostProcessing/VolumetricLights/index.tsx
+++ src/Realm/properties/PostProcessing/VolumetricLights/index.tsx
@@ -1,9 +1,5 @@
-import {Vector3} from "three";
-import {useEffect, useRef} from "react";
-import {useFrame} from "@react-three/fiber";
 import { SpotLight, useDepthBuffer } from "@react-three/drei"
 import {usePlayer} from "spacesvr";
-import * as THREE from "three";
 
 export default function VolumetricLights() {
   const depthBuffer = useDepthBuffer({ frames: 1 });
@@ -15,19 +11,4 @@ export default function VolumetricLights() {
       <SpotLight depthBuffer={depthBuffer} color="#b00c3f" position={[1, 3, 0]} />
     </group>
   )
-}
-
-function MovingSpot({ vec = new Vector3(), ...props }) {
-  const light = useRef<any>()
-  // const viewport = useThree((state) => state.viewport)
-  // useFrame((state) => {
-  //   light.current.target.position.lerp(vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0), 0.1)
-  //   light.current.target.updateMatrixWorld()
-  // })
-  // useEffect(() => {
-  //   if (!light.current) return;
-  //   light.current.target.position = new Vector3(0, 0, 0)
-  // }, [light.current])
-  // @ts-ignore
-  return <SpotLight castShadow ref={light} penumbra={1} distance={6} angle={0.35} attenuation={5} anglePower={4} intensity={2} {...props} />
 }
