d63766062a4c6208ea73e63fbe7c27bb4d3e1b3e
ianyimi <anyiminene@gmail.com>
2022-02-14 00:20:35 -0800

    new mirror mechanic... not complete

--- src/ideas/MagicMirror.tsx
+++ src/ideas/MagicMirror.tsx
@@ -5,33 +5,43 @@ import * as THREE from "three";
 
 export default function MagicMirror(props: { children: any, args?: [width: number | undefined, height: number | undefined] } & GroupProps) {
   const { children, args = [3, 3], ...restProps } = props;
-  const cam = useRef()
+  const cam = useRef();
+  const plane = useRef();
   // useFBO creates a WebGL2 buffer for us, it's a helper from the "drei" library
   const fbo = useFBO()
   // The is a separate scene that we create, React will portal into that
-  const [scene] = useState(() => new THREE.Scene())
+  const [newScene] = useState(() => new THREE.Scene())
   // Tie this component into the render-loop
 
+  const { scene } = useThree();
+  const tempCam = new THREE.PerspectiveCamera(50, 1, 0.1, 500);
+  tempCam.position.set(-10, 1, 0);
+  // tempCam.rotation.set(0, -Math.PI/2, 0)
+  scene.add(tempCam);
+
   useFrame((state) => {
+    tempCam.position.set(tempCam.position.x, 1, state.camera.position.z);
+    tempCam.lookAt(plane?.current.position.x, 1, plane?.current.position.z);
     // Our portal has its own camera, but we copy the originals world matrix
-    cam.current?.matrixWorldInverse.copy(state.camera.matrixWorldInverse)
+    cam.current?.matrixWorldInverse.copy(tempCam.matrixWorldInverse)
     // Then we set the render-target to the buffer that we have created
     state.gl.setRenderTarget(fbo)
     // We render the scene into it, using the local camera that is clamped to the planes aspect ratio
-    state.gl.render(scene, cam.current)
+    state.gl.render(newScene, cam.current)
     // And flip the render-target to the default again
     state.gl.setRenderTarget(null)
   })
+
   return (
     <>
-      <mesh {...restProps}>
+      <mesh ref={plane} {...restProps}>
         <planeGeometry args={args} />
         {/* The "mirror" is just a boring plane, but it receives the buffer texture */}
         <meshBasicMaterial map={fbo.texture} />
       </mesh>
       <PerspectiveCamera manual ref={cam} fov={30} aspect={args[0] / args[1]} onUpdate={(c) => c.updateProjectionMatrix()} />
       {/* This is React being awesome, we portal this components children into the separate scene above */}
-      {createPortal(children, scene)}
+      {createPortal(children, newScene)}
     </>
   )
 }
