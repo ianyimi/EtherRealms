30e938f30994fa0ce79f65fd3b91e5d4e87d551b
ianyimi <anyiminene@gmail.com>
2022-03-07 20:42:12 -0800

    attempted mc model w many textures and collisions

--- src/worlds/Basketball/components/Environment.tsx
+++ src/worlds/Basketball/components/Environment.tsx
@@ -1,13 +1,15 @@
 import Court from "../models/Court6";
 import { Stars } from "@react-three/drei";
 import Torus from "./Torus";
+import Fgdbg2 from "../models/Fgdbg2";
 
 export default function Environment() {
   return (
     <group>
-      <Court scale={0.5} position-y={-2} />
+      {/*<Court scale={0.5} position-y={-2} />*/}
       <Stars count={1000} radius={0.5} fade />
-      <Torus />
+      {/*<Torus />*/}
+      <Fgdbg2 position-y={-10} />
     </group>
   )
 }
