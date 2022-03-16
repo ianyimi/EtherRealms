5313874880fcbf5c3266936169f4d9682f4f7563
ianyimi <anyiminene@gmail.com>
2022-03-16 01:03:03 -0700

    model imported

--- src/DivineRealms/Cribs/index.tsx
+++ src/DivineRealms/Cribs/index.tsx
@@ -1,20 +1,22 @@
 import { StandardEnvironment } from "spacesvr";
 import Cribs from "./models/Cribs";
 import { Stars } from "@react-three/drei";
+import { Debug } from "@react-three/cannon";
 
 export default function Home() {
   return (
     <StandardEnvironment
       dev={process.env.NODE_ENV === "development"}
       canvasProps={{ camera: { far: 1000 } }}
-      playerProps={{ pos: [120, 1, 80], rot: Math.PI/2, speed: 10, controls: { disableGyro: true } }}
+      playerProps={{ pos: [0, 1, 0], speed: 10, controls: { disableGyro: true } }}
       physicsProps={{ defaultContactMaterial: { friction: 0.01 } }}
       // disableGround
     >
-      {/*<Debug scale={1} color="red">*/}
-      <Stars />
-      <Cribs />
-      {/*</Debug>*/}
+      <Debug scale={1} color="red">
+        <Stars />
+        <Cribs position-y={-32} />
+        <ambientLight intensity={1} />
+      </Debug>
     </StandardEnvironment>
   );
 }
