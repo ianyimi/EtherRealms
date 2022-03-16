4bd117b8d3002c6cf718815b81f3f8097a65f9dc
ianyimi <anyiminene@gmail.com>
2022-03-16 12:48:22 -0700

    etherrealms pause menu added to cribs scene

--- src/DivineRealms/Cribs/index.tsx
+++ src/DivineRealms/Cribs/index.tsx
@@ -2,6 +2,8 @@ import { StandardEnvironment } from "spacesvr";
 import Cribs from "./models/Cribs";
 import { Stars } from "@react-three/drei";
 import { Debug } from "@react-three/cannon";
+import PauseMenu from "styles/PauseMenu";
+
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       canvasProps={{ camera: { far: 1000 } }}
       playerProps={{ pos: [0, 1, 50], speed: 10, controls: { disableGyro: true } }}
       physicsProps={{ defaultContactMaterial: { friction: 0.01 } }}
+      pauseMenu={<PauseMenu />}
       // disableGround
     >
       <Debug scale={1} color="red">
