635b62d01d016a17a40693465d703661f50108e8
ianyimi <anyiminene@gmail.com>
2022-04-30 22:30:29 -0700

    collisions v1

--- src/DivineRealms/WinterTemple/index.tsx
+++ src/DivineRealms/WinterTemple/index.tsx
@@ -2,24 +2,27 @@ import { StandardEnvironment } from "spacesvr";
 import { Sky } from "@react-three/drei"
 import PauseMenu from "styles/PauseMenu";
 import Water from "../../ideas/Water";
-import Temple1 from "./models/Temple1";
+import Temple from "./models/Temple3";
+import { Debug } from "@react-three/cannon";
 
 export default function WinterTemple() {
   return (
     <StandardEnvironment
       dev={process.env.NODE_ENV === "development"}
       canvasProps={{ camera: { far: 1000 } }}
-      playerProps={{ pos: [12, 1, 40], speed: 5, controls: { disableGyro: true } }}
+      playerProps={{ pos: [10, 2, 21], speed: 5, controls: { disableGyro: true } }}
       physicsProps={{ defaultContactMaterial: { friction: 0.01 } }}
       pauseMenu={<PauseMenu />}
       // disableGround
     >
-      <group position-y={-1}>
-        <Temple1 />
-        <Water color="0x001e0f" speed={0.25} />
-      </group>
-      <Sky sunPosition={0.75} />
-      <ambientLight />
+      {/*<Debug color="red" scale={1}>*/}
+        <group position-y={-1}>
+          <Temple />
+          <Water color="0x001e0f" speed={0.25} />
+        </group>
+        <Sky sunPosition={0.75} />
+        <ambientLight />
+      {/*</Debug>*/}
     </StandardEnvironment>
   )
 }
